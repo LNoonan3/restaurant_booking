353dc29d0c3d9d2ef6211a176e183accef357b2a
LNoonan3 <lucynoonan3@gmail.com>
2025-03-10 16:07:14 +0000

    fix deployment

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'booking', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -61,6 +60,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'booking',
+    'whitenoise',
 ]
 
 SITE_ID = 1
