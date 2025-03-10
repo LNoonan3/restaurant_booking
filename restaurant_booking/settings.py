94de5470fb7ff1d16c3facb861fc4399f0de4c00
LNoonan3 <lucynoonan3@gmail.com>
2025-03-10 16:07:29 +0000

    fix deploment

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',  # Required for django-allauth
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
